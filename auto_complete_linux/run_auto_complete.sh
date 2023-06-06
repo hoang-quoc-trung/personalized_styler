5e1f7aaeae14f791291b1edd8be28a5d2e658715
Trung Hoang <hoangtrung020541@gmail.com>
2023-06-07 00:49:37 +0700

    feat: create auto-complete configuration for terminal using Bash Line Editor on Linux

---
+++ auto_complete_linux/run_auto_complete.sh
@@ -0,0 +1,11 @@
+# Auto Complete ble.sh
+
+#git clone --recursive https://github.com/akinomyoga/ble.sh.git
+cd ble.sh
+sudo apt-get update
+sudo apt-get install gawk
+make install
+make install INSDIR=/path/to/blesh
+make install DESTDIR=/tmp/blesh-package PREFIX=/usr/local
+echo '[[ $- == *i* ]] && source '"$PWD"'/out/ble.sh --noattach' >> /root/.bashrc
+echo '[[ ${BLE_VERSION-} ]] && ble-attach' >> /root/.bashrc
